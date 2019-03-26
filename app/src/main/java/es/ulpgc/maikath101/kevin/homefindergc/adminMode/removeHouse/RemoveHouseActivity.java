5a0dda6a63e4ad45c35cd77fafe193d96a67f35e
Manuel <msalaguila@gmail.com>
2019-03-26 08:17:34 +0000

    RemoveHouseScreen done

--- app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/adminMode/removeHouse/RemoveHouseActivity.java
+++ app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/adminMode/removeHouse/RemoveHouseActivity.java
@@ -2,7 +2,11 @@ package es.ulpgc.maikath101.kevin.homefindergc.adminMode.removeHouse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import es.ulpgc.maikath101.kevin.homefindergc.R;
 
@@ -13,10 +17,28 @@ public class RemoveHouseActivity
 
   private RemoveHouseContract.Presenter presenter;
 
+
+
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_add_house);
+    setContentView(R.layout.activity_remove_house);
+
+    Toolbar toolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+
+
+
+    Button toolbarButton = toolbar.findViewById(R.id.toolbarButton);
+    TextView toolbarTextView = toolbar.findViewById(R.id.toolbarTextView);
+
+    toolbarButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Toast.makeText(getApplicationContext(),"Esto es una prueba", Toast.LENGTH_LONG).show();
+      }
+    });
+
 
     // do the setup
     RemoveHouseScreen.configure(this);
