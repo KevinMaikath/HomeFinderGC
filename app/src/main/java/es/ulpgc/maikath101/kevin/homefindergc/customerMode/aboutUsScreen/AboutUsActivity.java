4dcee5fd968a84ac3397446803c78ddca4e80578
Kevin Maikath <kevinrx25@gmail.com>
2019-03-29 12:35:15 +0000

    Implementado el cambio entre pantallas a través del Drawer

--- app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/aboutUsScreen/AboutUsActivity.java
+++ app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/aboutUsScreen/AboutUsActivity.java
@@ -1,24 +1,50 @@
 package es.ulpgc.maikath101.kevin.homefindergc.customerMode.aboutUsScreen;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import es.ulpgc.maikath101.kevin.homefindergc.R;
 import es.ulpgc.maikath101.kevin.homefindergc.app.FlowTextHelper;
 
-public class AboutUsActivity extends AppCompatActivity implements AboutUsContract.View {
+public class AboutUsActivity extends AppCompatActivity implements AboutUsContract.View, NavigationView.OnNavigationItemSelectedListener {
   public static String TAG = AboutUsActivity.class.getSimpleName();
 
   private AboutUsContract.Presenter presenter;
+  private FloatingActionButton floatingActionButton;
+  private DrawerLayout drawerLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_about_us);
+
+    floatingActionButton = findViewById(R.id.floatingOptionsButton);
+    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        drawerLayout.openDrawer(GravityCompat.START);
+      }
+    });
+    drawerLayout = findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawerLayout.addDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = findViewById(R.id.navigation_view);
+    navigationView.setNavigationItemSelectedListener(this);
+
 
     // do the setup
     AboutUsScreen.configure(this);
@@ -50,4 +76,37 @@ public class AboutUsActivity extends AppCompatActivity implements AboutUsContrac
 
     // deal with the data}
   }
+
+  @Override
+  public void onBackPressed() {
+    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+      drawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if (id == R.id.nav_inicio) {
+      presenter.goToStart();
+    } else if (id == R.id.nav_se_vende) {
+      presenter.goToForSale();
+    } else if (id == R.id.nav_se_alquila) {
+      presenter.goToForRent();
+    } else if (id == R.id.nav_alquiler_vacacional) {
+      presenter.goToHolidayRental();
+    } else if (id == R.id.nav_sobre_nosotros) {
+      presenter.goToAboutUS();
+    } else if (id == R.id.nav_contacto) {
+      presenter.goToContact();
+    }
+
+    drawerLayout.closeDrawer(GravityCompat.START);
+    return true;
+  }
+
 }
--- app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/homeDetails/HomeDetailsActivity.java
+++ app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/homeDetails/HomeDetailsActivity.java
@@ -1,23 +1,51 @@
 package es.ulpgc.maikath101.kevin.homefindergc.customerMode.homeDetails;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import es.ulpgc.maikath101.kevin.homefindergc.R;
 
 public class HomeDetailsActivity
-        extends AppCompatActivity implements HomeDetailsContract.View {
+        extends AppCompatActivity implements HomeDetailsContract.View, NavigationView.OnNavigationItemSelectedListener{
 
   public static String TAG = HomeDetailsActivity.class.getSimpleName();
 
   private HomeDetailsContract.Presenter presenter;
+  private FloatingActionButton floatingActionButton;
+  private DrawerLayout drawerLayout;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home_details);
+
+    floatingActionButton = findViewById(R.id.floatingOptionsButton);
+    floatingActionButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        drawerLayout.openDrawer(GravityCompat.START);
+      }
+    });
+    drawerLayout = findViewById(R.id.drawer_layout);
+    ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+            this, drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    drawerLayout.addDrawerListener(toggle);
+    toggle.syncState();
+
+    NavigationView navigationView = findViewById(R.id.navigation_view);
+    navigationView.setNavigationItemSelectedListener(this);
+
+
+
 
     // do the setup
     HomeDetailsScreen.configure(this);
@@ -42,4 +70,39 @@ public class HomeDetailsActivity
 
     // deal with the data
   }
+
+
+  @Override
+  public void onBackPressed() {
+    if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+      drawerLayout.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if (id == R.id.nav_inicio) {
+      presenter.goToStart();
+    } else if (id == R.id.nav_se_vende) {
+      presenter.goToForSale();
+    } else if (id == R.id.nav_se_alquila) {
+      presenter.goToForRent();
+    } else if (id == R.id.nav_alquiler_vacacional) {
+      presenter.goToHolidayRental();
+    } else if (id == R.id.nav_sobre_nosotros) {
+      presenter.goToAboutUS();
+    } else if (id == R.id.nav_contacto) {
+      presenter.goToContact();
+    }
+
+    drawerLayout.closeDrawer(GravityCompat.START);
+    return true;
+  }
+
 }
