6d652c29abbeafd49ba0d4358d4446bd3a47c4c0
Manuel <msalaguila@gmail.com>
2019-04-29 09:57:31 +0100

    Contact Screen

--- app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/contactScreen/ContactRouter.java
+++ app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/contactScreen/ContactRouter.java
@@ -2,6 +2,9 @@ package es.ulpgc.maikath101.kevin.homefindergc.customerMode.contactScreen;
 
 import android.content.Intent;
 import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.Toast;
 
 import es.ulpgc.maikath101.kevin.homefindergc.app.AppCustomerRouter;
 import es.ulpgc.maikath101.kevin.homefindergc.app.AppMediator;
@@ -15,7 +18,36 @@ public class ContactRouter extends AppCustomerRouter implements ContactContract.
   }
 
   @Override
-  public void sendEmail(String name, String telephone, String country, String zipcode, String address, String email, String subject, String bodyemail) {
+  public void sendEmail(String name, String telephone, String country, String zipcode,
+                        String address, String email, String subject, String bodyemail) {
+    String[] TO = {"msalaguilo@gmail.com",
+    //        "oliver@homefindergc.com"
+    }; //Direcciones email  a enviar.
+    String[] CC = {""}; //Direcciones email con copia.
+
+    Context context = mediator.getApplicationContext();
+    Intent emailIntent = new Intent(Intent.ACTION_SEND);
+
+    emailIntent.setData(Uri.parse("mailto:"));
+    emailIntent.setType("text/plain");
+    emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+    emailIntent.putExtra(Intent.EXTRA_CC, CC);
+    emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+    emailIntent.putExtra(Intent.EXTRA_TEXT, "Nombre: " + name +" \n\n " +
+            "Teléfono de contacto: " + telephone + " \n\n " +
+            "País: " + country + " \n\n " +
+            "Código Postal: " + zipcode + " \n\n " +
+            "Dirección: " + address + " \n\n " +
+            "Dirección de contacto: " + email + " \n\n " +
+            bodyemail); // * configurar email aquí!
+
+    try {
+      context.startActivity(Intent.createChooser(emailIntent, "Enviar email."));
+      Log.i("EMAIL", "Enviando email...");
+    }
+    catch (android.content.ActivityNotFoundException e) {
+      // Toast.makeText(this, "NO existe ningún cliente de email instalado!.", Toast.LENGTH_SHORT).show();
+    }
 
   }
 }
