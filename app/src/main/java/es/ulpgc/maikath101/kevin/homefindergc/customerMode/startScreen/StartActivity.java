faa546609091c895779dcd2867d8686592b4c0a6
Kevin Maikath <kevinrx25@gmail.com>
2019-04-29 08:58:01 +0100

    StartActivity hace scroll entera, el recycler ya no puede

--- app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/startScreen/StartActivity.java
+++ app/src/main/java/es/ulpgc/maikath101/kevin/homefindergc/customerMode/startScreen/StartActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.MenuItem;
@@ -76,6 +77,14 @@ public class StartActivity
 
     RecyclerView recyclerView = findViewById(R.id.recyclerView);
     recyclerView.setAdapter(listAdapter);
+
+    RecyclerView.LayoutManager linearLayoutManager = new LinearLayoutManager(this) {
+      @Override
+      public boolean canScrollVertically() {
+        return false;
+      }
+    };
+    recyclerView.setLayoutManager(linearLayoutManager);
 
     // do the setup
     StartScreen.configure(this);
