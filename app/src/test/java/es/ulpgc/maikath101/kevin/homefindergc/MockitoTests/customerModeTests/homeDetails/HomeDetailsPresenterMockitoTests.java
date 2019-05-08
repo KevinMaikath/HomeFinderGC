f770e57a29dbd92722f39f9ecc3cd730a1e4ef98
Kevin Maikath <kevinrx25@gmail.com>
2019-05-08 18:36:25 +0100

    Implementando Mockito tests

--- app/src/test/java/es/ulpgc/maikath101/kevin/homefindergc/MockitoTests/customerModeTests/homeDetails/HomeDetailsPresenterMockitoTests.java
+++ app/src/test/java/es/ulpgc/maikath101/kevin/homefindergc/MockitoTests/customerModeTests/homeDetails/HomeDetailsPresenterMockitoTests.java
@@ -10,6 +10,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 
+import es.ulpgc.maikath101.kevin.homefindergc.customerMode.homeDetails.HomeDetailsActivity;
 import es.ulpgc.maikath101.kevin.homefindergc.customerMode.homeDetails.HomeDetailsContract;
 import es.ulpgc.maikath101.kevin.homefindergc.customerMode.homeDetails.HomeDetailsPresenter;
 import es.ulpgc.maikath101.kevin.homefindergc.customerMode.homeDetails.HomeDetailsState;
@@ -17,6 +18,8 @@ import es.ulpgc.maikath101.kevin.homefindergc.data.House;
 import es.ulpgc.maikath101.kevin.homefindergc.data.Image;
 import es.ulpgc.maikath101.kevin.homefindergc.data.SellHouse;
 
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -42,7 +45,7 @@ public class HomeDetailsPresenterMockitoTests {
     MockitoAnnotations.initMocks(this);
   }
 
-  private void configureStartScreen(HomeDetailsState state) {
+  private void configureHomeDetailsScreen(HomeDetailsState state) {
 
     // Get a reference to the class under test
     presenter = new HomeDetailsPresenter(state);
@@ -61,10 +64,7 @@ public class HomeDetailsPresenterMockitoTests {
             "", "", 3, "");
     state.image_list = new ArrayList<Image>();
     state.sellHouse = new SellHouse(1, 1, "");
-    configureStartScreen(state);
-
-    // When start a new screen is requested
-    when(modelMock.getSummaryInfo()).thenReturn("Summary");
+    configureHomeDetailsScreen(state);
 
     presenter.fetchData();
 
@@ -72,6 +72,59 @@ public class HomeDetailsPresenterMockitoTests {
     verify(modelMock).setSellHouse(state.sellHouse);
 
     verify(viewMock).setImageList(state);
+    verify(viewMock).displayData(state);
+  }
+
+  @Test
+  public void summaryButtonClicked(){
+    HomeDetailsState state = new HomeDetailsState();
+    configureHomeDetailsScreen(state);
+
+    presenter.summaryButtonClicked();
+
+    verify(modelMock).getSummaryInfo();
+    verify(viewMock).displayData(state);
+  }
+
+  @Test
+  public void descriptionButtonClicked(){
+    HomeDetailsState state = new HomeDetailsState();
+    configureHomeDetailsScreen(state);
+    when(modelMock.getHouse()).thenReturn( new House(1, 1, 1, "",
+            "", "", "", 1, "", false, 2,
+            "", "", 3, "") );
+
+    presenter.descriptionButtonClicked();
+
+    verify(modelMock).getHouse();
+    verify(viewMock).displayData(state);
+  }
+
+  @Test
+  public void locationButtonClicked(){
+    HomeDetailsState state = new HomeDetailsState();
+    configureHomeDetailsScreen(state);
+    when(modelMock.getHouse()).thenReturn( new House(1, 1, 1, "",
+            "", "", "", 1, "", false, 2,
+            "", "", 3, "") );
+
+    presenter.locationButtonClicked();
+
+    verify(modelMock).getHouse();
+    verify(viewMock).displayData(state);
+  }
+
+  @Test
+  public void distributionButtonClicked(){
+    HomeDetailsState state = new HomeDetailsState();
+    configureHomeDetailsScreen(state);
+    when(modelMock.getHouse()).thenReturn( new House(1, 1, 1, "",
+            "", "", "", 1, "", false, 2,
+            "", "", 3, "") );
+
+    presenter.distributionButtonClicked();
+
+    verify(modelMock).getHouse();
     verify(viewMock).displayData(state);
   }
 
