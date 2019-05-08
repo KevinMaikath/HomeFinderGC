3776a0af3f5fd514149036614d9eb68d76f140e6
Kevin Maikath <kevinrx25@gmail.com>
2019-05-08 17:22:40 +0100

    Implementando JUnit tests

--- app/src/test/java/es/ulpgc/maikath101/kevin/homefindergc/JUnitTests/ExampleUnitTest.java
+++ app/src/test/java/es/ulpgc/maikath101/kevin/homefindergc/JUnitTests/ExampleUnitTest.java
@@ -1,6 +1,9 @@
 package es.ulpgc.maikath101.kevin.homefindergc.JUnitTests;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import es.ulpgc.maikath101.kevin.homefindergc.data.SimpleHouse;
 
 import static org.junit.Assert.*;
 
@@ -10,8 +13,13 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
-    }
+
+  @Test
+  public void checkForSaleAsForSale() {
+    SimpleHouse house = new SimpleHouse(1, "Ref", "name",
+            "url", "price", 111, "uri");
+
+
+    assertTrue();
+  }
 }
